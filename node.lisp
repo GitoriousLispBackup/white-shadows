3759ba4863e49da9f8cc30fc1656c23b68c9a88a
h2s04-KIsLotnIK <kisl0tneg@gmail.com>
2010-04-06 20:43:03 -0400

    Initial commit

---
+++ node.lisp
@@ -0,0 +1,110 @@
+(in-package :common-lisp-user)
+
+(defpackage h2s04.white-shadow-node
+  (:nicknames :ws-node)
+  (:use :common-lisp
+	:common-lisp-user
+	:sb-bsd-sockets))
+
+
+(in-package :ws-node)
+
+
+
+(defparameter *port* 30033)
+
+(asdf:operate 'asdf:load-op 'cl-store)
+
+
+(defun make-echoer (stream id disconnector)
+  (lambda (_)
+    (declare (ignore _))
+    (handler-case
+        (let ((cmd (read stream)))
+          ;;(setf line (subseq line 0 (1- (length line))))
+	  (cond ((equal cmd '(quit))
+                 (funcall disconnector))
+                (t
+		 (format t "evaling: ~a~%" cmd)
+		 (let ((result (eval cmd)))
+		   (format t "~a: ~a~%" id result)
+		   (format stream "~a: ~a~%" id result))
+                 (force-output stream))))
+      (end-of-file ()
+        (funcall disconnector)))))
+
+(defun make-disconnector (socket id)
+  (lambda ()
+    (let ((fd (socket-file-descriptor socket)))
+      (format t "~a: closing~%" id)
+      (sb-impl::invalidate-descriptor fd)
+      (socket-close socket))))
+
+(defun serve (socket id)
+  (let ((stream (socket-make-stream socket :output t :input t))
+        (fd (socket-file-descriptor socket)))
+    (sb-impl::add-fd-handler fd
+                             :input
+                             (make-echoer stream
+                                          id
+                                          (make-disconnector socket id)))))
+
+(defun echo-server (&optional (port *port*))
+  (let ((socket (make-instance 'inet-socket :type :stream :protocol :tcp))
+        (counter 0))
+    (socket-bind socket (vector 127 0 0 1) port)
+    (socket-listen socket 5)
+    (sb-impl::add-fd-handler (socket-file-descriptor socket)
+                             :input
+                             (lambda (_)
+                               (declare (ignore _))
+                               (incf counter)
+                               (format t "Accepted client ~A~%" counter)
+                               (serve (socket-accept socket) counter)))))
+
+#+sb-thread
+(sb-thread:make-thread (lambda ()
+                         (echo-server)
+                         (loop
+                            (sb-impl::serve-all-events))))
+#-sb-thread
+(echo-server)
+
+
+;; as a tool
+
+(defun nslookup (hostname)
+  "Performs a DNS look up for HOSTNAME and returns the address as a
+  four element array, suitable for socket-connect. If HOSTNAME is
+  not found, a host-not-found-error condition is thrown."
+  (if hostname
+      (host-ent-address (get-host-by-name hostname))
+      nil))
+
+
+;; connect to a server
+
+(defun tcp-connect (server port &optional (timeout 5))
+  "Returns a socket connected to SERVER:PORT. If an error occurs, or the attempt times out
+  after TIMOUT (default 5) secons, nil is returned."
+  (when (and server port)
+    (handler-case
+	(let ((socket (make-instance 'sb-bsd-sockets:inet-socket :type :stream :protocol :tcp)))
+	  (socket-connect socket (nslookup server) port)
+	  socket)
+      (host-not-found-error ()
+	(format t "host ~A not found." server)
+	(force-output)
+	nil))))
+
+(defparameter *node1* (tcp-connect "127.0.01" 30033))
+
+
+
+(defun send-task (socket task)
+  (let ((task-str (concatenate 'string (prin1-to-string '(+ 15 25)) (list #\return #\newline))))
+	   (sb-bsd-sockets:socket-send ws-node::*node1* cmd (length cmd))))
+
+;;(socket-send socket
+;;	       (concatenate 'string task (list #\return #\newline))
+;;	       (+ (length task) 2))
---
+++ user-interface.lisp
@@ -0,0 +1,26 @@
+(in-package :common-lisp-user)
+
+(defpackage has04.white-shadow-ui
+  (:nicknames :ws-ui)
+  (:use :common-lisp :common-lisp-user))
+
+
+(in-package :ws-ui)
+
+
+
+(defparameter *max-thread-quantity-mutex*
+  (sb-thread:make-mutex :name "threads quantity lock"))
+(defparameter *max-threads-quantity* 1 "Maximum number of threads")
+
+
+(defun set-max-threads-count (number)
+  (assert (> number 0) (number) "Number of threads must be greater than zero")
+  (sb-thread:with-mutex (*max-thread-quantity-mutex*)
+    (setf *max-threads-quantity* number)))
+
+
+(defun add-task (task)
+  (setf *tasks-pool*
+	(nconc *tasks-pool* task))
+  (grab-new-task *tasks-pool* *threads-pool*))
